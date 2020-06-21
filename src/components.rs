510b2716c39e117edc0ae24ecc157c8db7cbe97c
Diamondlord <vladimir.almazov@gmail.com>
2020-06-22 01:34:22 +0300

    map with rooms

---
+++ src/components.rs
@@ -0,0 +1,23 @@
+use specs::prelude::*;
+use specs_derive::*;
+use rltk::{RGB};
+
+#[derive(Component)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+// impl Component for Position {
+//     type Storage = VecStorage<Self>;
+// }
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: rltk::FontCharType,
+    pub fg: RGB,
+    pub bg: RGB,
+}
+
+#[derive(Component, Debug)]
+pub struct Player {}
