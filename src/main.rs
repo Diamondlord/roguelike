630e5dc341fdf999da4a22db1f668360a217e7a3
Diamondlord <vladimir.almazov@gmail.com>
2020-06-25 23:36:04 +0300

    add spawner

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ mod gui;
 use gui::*;
 mod gamelog;
 use gamelog::*;
+mod spawner;
+use spawner::*;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum RunState { AwaitingInput, PreRun, PlayerTurn, MonsterTurn }
@@ -100,7 +102,7 @@ fn main() -> rltk::BError {
         .with_title("Roguelike")
         .build()?;
     // add burn effect
-    context.with_post_scanlines(true);
+    // context.with_post_scanlines(true);
     let mut gs = State {
         ecs: World::new(),
     };
@@ -115,54 +117,17 @@ fn main() -> rltk::BError {
     gs.ecs.register::<WantsToMelee>();
     gs.ecs.register::<SufferDamage>();
 
-
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
     gs.ecs.insert(Point::new(player_x, player_y));
     gs.ecs.insert(RunState::PreRun);
     gs.ecs.insert(GameLog { entries : vec!["Welcome to Rusty Roguelike".to_string()] });
-    let mut rng = rltk::RandomNumberGenerator::new();
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
     for (i, room) in map.rooms.iter().enumerate().skip(1) {
-        let (x,y) = room.center();
-
-        let glyph: rltk::FontCharType;
-        let name: String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
-            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
-        }
-
-        gs.ecs.create_entity()
-            .with(Position{ x, y })
-            .with(Renderable {
-                glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-            .with(Monster{})
-            .with(BlocksTile{})
-            .with(Name{ name: format!("{} #{}", &name, i) })
-            .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
-            .build();
+        spawner::spawn_room(&mut gs.ecs, room);
     };
-
     gs.ecs.insert(map);
-
-    let player_entity = gs.ecs
-        .create_entity()
-        .with(Position { x: player_x, y: player_y })
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player{})
-        .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
-        .with(Name{name: "Player".to_string() })
-        .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5 })
-        .build();
 
     gs.ecs.insert(player_entity);
 
